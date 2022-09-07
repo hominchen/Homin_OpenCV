3cc4f7d8ce2825afec7cb16ba891212e72c4a456
unknown <chenhomin2020@gmail.com>
2022-09-07 09:38:52 +0800

    pic show

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from fileinput import filename
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,11 +10,15 @@ import uuid as uuid
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = 'static/images'
+# UPLOAD_FOLDER = 'static/images'
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+UPLOAD_FOLDER = os.path.join('static', 'images')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 app.config['SECRET_KEY'] = "secret key 123456987"
 
-
+# 因為首頁有使用form上傳，所以要另外加一個route
 @app.route("/")
 def hello():
     return render_template('index.html')
@@ -33,6 +36,14 @@ def show_img_OpenCV():
     return render_template('index.html', 
         image_path=image_path)
 
+# 在Flask應用程式中顯示多個影象
+@app.route('/pics_show', methods=['GET', 'POST'])
+def pics_show():
+    IMG_LIST = os.listdir('static/images')
+    print(IMG_LIST)
+    IMG_LIST = ['images/' + i for i in IMG_LIST]
+    print(IMG_LIST)
+    return render_template("pics_show.html", imagelist=IMG_LIST)
 
 
 if __name__ == "__main__":
